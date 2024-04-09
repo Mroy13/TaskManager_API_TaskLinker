f6bf1ed3532563d1acef44afd36587f1cf5d4ec3
Mroy13 <mtsprotechguide@gmail.com>
2024-04-10 03:30:18 +0530

    Added User model

---
+++ src/models/user.js
@@ -0,0 +1,47 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    UserName: {
+      type:DataTypes.STRING,
+      allowNull:false,
+      unique:true,
+
+    },
+    Name:{
+      type:DataTypes.STRING,
+      allowNull:false
+
+    },
+    email: {
+      type:DataTypes.STRING,
+      unique:true,
+      allowNull:false,
+      validate:{
+        isEmail:true
+      }
+    },
+    password: {
+      type:DataTypes.STRING,
+      validate:{
+       len:[3,50]
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
